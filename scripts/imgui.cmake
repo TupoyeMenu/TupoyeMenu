e01c74178a902bb8fd92a5ab2a7118e7e32ed7b1
Clone69420 <108097189+Clone69420@users.noreply.github.com>
2022-11-17 23:27:24 +0100

    Add files via upload

---
+++ scripts/imgui.cmake
@@ -0,0 +1,29 @@
+include(FetchContent)
+FetchContent_Declare(
+    imgui
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_TAG        4d5da74c7994157411744fc8a8705299ea8dc8ae
+    GIT_PROGRESS TRUE
+)
+message("ImGui")
+FetchContent_GetProperties(imgui)
+if(NOT imgui_POPULATED)
+    FetchContent_Populate(imgui)
+
+    file(GLOB SRC_IMGUI
+        "${imgui_SOURCE_DIR}/*.cpp"
+        "${imgui_SOURCE_DIR}/*.h"
+        "${imgui_SOURCE_DIR}/backends/imgui_impl_win32.*"
+        "${imgui_SOURCE_DIR}/backends/imgui_impl_dx11.*"
+        "${imgui_SOURCE_DIR}/misc/cpp/imgui_stdlib.*"
+    )
+
+    add_library(imgui STATIC ${SRC_IMGUI})
+    source_group(TREE ${imgui_SOURCE_DIR} PREFIX "imgui" FILES ${SRC_IMGUI} )
+    target_include_directories(imgui PRIVATE
+        "${imgui_SOURCE_DIR}"
+        "${imgui_SOURCE_DIR}/backends"
+        "${imgui_SOURCE_DIR}/misc/cpp"
+    )
+endif()
+set_property(TARGET imgui PROPERTY CXX_STANDARD 23)
