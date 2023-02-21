de9d3210ce93775e66c16a6194b35fa70f08f4d4
tupoy-ya <tupoy-ya@users.noreply.github.com>
2023-02-21 20:02:25 +0500

    fix(Chat log): Fix crashes when receiving chat.

--- src/util/spam.hpp
+++ src/util/spam.hpp
@@ -71,10 +71,10 @@ namespace big::spam
 		std::ofstream spam_log(g_file_manager->get_project_file(is_spam ? "./spam.log" : "./chat.log").get_path(), std::ios::app);
 
 		auto& plData = *player->get_net_data();
-		auto ip = g_player_service->get_selected()->get_ip_address();
+		netAddress ip = player->get_ip_address();
 
 		spam_log << player->get_name() << " (" << plData.m_gamer_handle.m_rockstar_id << ") <"
-			<< ip.m_field1 << "." << ip.m_field2 << "." << ip.m_field3 << "." << ip.m_field4 <<
+			<< (int)ip.m_field1 << "." << (int)ip.m_field2 << "." << (int)ip.m_field3 << "." << (int)ip.m_field4 <<
 			">: " << msg <<
 			std::endl;
 
