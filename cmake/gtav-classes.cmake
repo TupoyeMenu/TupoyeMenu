07abc28e0e59c31eeb53a1acdf49af9316fd8955
tupoy-ya <tupoy-ya@users.noreply.github.com>
2024-06-20 11:56:02 +0500

    chore(GTAV-Classess): Update GTAV-Classes.

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        c741a3031de7981d12c97f55569f930a82556d63
+    GIT_TAG        03b811b16a7b1697c4f156e426ca40aa31c1a5d5
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
